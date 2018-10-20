6a31b46e441d24ff7a413e4ab18595f701d93016
Boris Prosic <boris.prosiz@gmail.com>
2018-10-20 11:58:31 +0200

    Update CentralForm.cs

--- LittleProjectDemo/CentralForm.cs
+++ LittleProjectDemo/CentralForm.cs
@@ -3,8 +3,6 @@
  * User: Elektrolit
  * Date: 11.9.2018.
  * Time: 9:19
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
 using System.Collections.Generic;
@@ -19,7 +17,7 @@ using System.Windows.Forms;
 namespace LittleProjectDemo
 {
 	/// <summary>
-	/// Description of CentralForm.
+	/// Main program
 	/// </summary>
 	public partial class CentralForm : Form
 	{
