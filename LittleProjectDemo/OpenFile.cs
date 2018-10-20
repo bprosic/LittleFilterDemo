bec057a4d857e86af9e20474ac206eb1125d27cb
Boris Prosic <boris.prosiz@gmail.com>
2018-10-20 11:59:32 +0200

    Update OpenFile.cs

--- LittleProjectDemo/OpenFile.cs
+++ LittleProjectDemo/OpenFile.cs
@@ -4,7 +4,6 @@
  * Date: 11.9.2018.
  * Time: 9:20
  * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
 using System.Collections.Generic;
@@ -15,7 +14,7 @@ using System.Windows.Forms;
 namespace LittleProjectDemo
 {
 	/// <summary>
-	/// maybe later i will use this class for other file or sth else..
+	/// Open txt file 
 	/// </summary>
 	public class OpenFile
 	{
